4f6742799052bf0919444d0c4048604cf90ff564
camilazci <camilazucchi@outlook.com>
2024-03-11 16:54:15 -0300

    feat: utilize RETURN_GENERATED_KEYS and getGeneratedKeys for retrieving auto-generated keys

--- src/application/InsertData.java
+++ src/application/InsertData.java
@@ -3,16 +3,14 @@ package application;
 import db.DB;
 import db.DbException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 /* Demo: Inserir dados
  * Checklist:
  * (✓) Inserção simples com PreparedStatement
- * () Inserção com recuperação de ID */
+ * (✓) Inserção com recuperação de ID */
 public class InsertData {
     public static void main(String[] args) {
         PreparedStatement preparedStatement = null;
@@ -21,7 +19,7 @@ public class InsertData {
         try {
             try (Connection connection = DB.getConnection()) {
                 String sql = "INSERT INTO seller (name, email, birthdate, basesalary, departmentid) VALUES (?, ?, ?, ?, ?)";
-                preparedStatement = connection.prepareStatement(sql);
+                preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
                 preparedStatement.setString(1, "Carl Grimes");
                 preparedStatement.setString(2, "carl@gmail.com");
                 preparedStatement.setDate(3, new java.sql.Date(sdf.parse("22/04/1999").getTime()));
@@ -30,7 +28,16 @@ public class InsertData {
 
                 /* Quando há uma alteração que altera os dados, chamamos o método "executeUpdate()". */
                 int rowsAffected = preparedStatement.executeUpdate();
-                System.out.println("Done! Rows affected: " + rowsAffected);
+
+                if (rowsAffected > 0) {
+                    ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                    while (resultSet.next()) {
+                        int id = resultSet.getInt(1);
+                        System.out.println("Done! ID: " + id);
+                    }
+                } else {
+                    System.out.println("No rows were affected.");
+                }
             } catch (ParseException e) {
                 throw new DbException(e.getMessage());
             }
