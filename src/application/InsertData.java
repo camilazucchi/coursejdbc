03d68ac96ab7490bfa0fb2b098505f1a341546d7
camilazci <camilazucchi@outlook.com>
2024-03-12 14:21:33 -0300

    feat: add code comments

--- src/application/InsertData.java
+++ src/application/InsertData.java
@@ -13,12 +13,19 @@ import java.text.SimpleDateFormat;
  * (✓) Inserção com recuperação de ID */
 public class InsertData {
     public static void main(String[] args) {
+        /* Declaração da variável "preparedStatement", que será usada para preparar e executar uma instrução SQL
+         * parametrizada: */
         PreparedStatement preparedStatement = null;
+        /* Criação de um objeto "SimpleDateFormat" para formatação de datas no formato "dd/MM/yyyy": */
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         try {
+            // Obtém conexão com o banco de dados utilizando o método "DB.getConnection()":
             try (Connection connection = DB.getConnection()) {
+                // Declaração da string SQL que será usada para inserir dados na tabela "seller":
                 String sql = "INSERT INTO seller (name, email, birthdate, basesalary, departmentid) VALUES (?, ?, ?, ?, ?)";
+                /* Prepara a instrução SQL parametrizada e indica que desejamos recuperar as chaves auto-geradas após a
+                 * execução da instrução: */
                 preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
                 preparedStatement.setString(1, "Carl Grimes");
                 preparedStatement.setString(2, "carl@gmail.com");
@@ -26,11 +33,14 @@ public class InsertData {
                 preparedStatement.setDouble(4, 3000.0);
                 preparedStatement.setInt(5, 4);
 
-                /* Quando há uma alteração que altera os dados, chamamos o método "executeUpdate()". */
+                /* Quando há uma alteração que altera os dados, chamamos o método "executeUpdate()"!
+                * Executa a instrução SQL e retorna o número de linhas afetadas: */
                 int rowsAffected = preparedStatement.executeUpdate();
 
                 if (rowsAffected > 0) {
+                    // Recupera o conjunto de chaves auto-geradas após a execução da instrução SQL:
                     ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                    // Itera sobre o conjunto de chaves auto-geradas e imprime o ID correspondente:
                     while (resultSet.next()) {
                         int id = resultSet.getInt(1);
                         System.out.println("Done! ID: " + id);
@@ -45,7 +55,7 @@ public class InsertData {
             throw new DbException(e.getMessage());
         } finally {
             DB.closeStatement(preparedStatement);
-            // Sempre fechamos a conexão por último.
+            // Sempre fechamos a conexão por último!
             DB.closeConnection();
         }
     }
