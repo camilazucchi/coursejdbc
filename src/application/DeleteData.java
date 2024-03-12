b2cb4da755c216538ab91759eb12e82345340302
camilazci <camilazucchi@outlook.com>
2024-03-12 15:48:42 -0300

    feat: implementing functionality for deleting data from the data base

--- src/application/DeleteData.java
+++ src/application/DeleteData.java
@@ -1,11 +1,35 @@
 package application;
 
+import db.DB;
+import db.DbException;
+import db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static java.lang.System.*;
+
 /* Demo: Deletar dados
 * Checklist:
-* () Criar DbIntegrityException
-* () Tratar a exceção de integridade referencial */
+* (✓) Criar DbIntegrityException
+* (✓) Tratar a exceção de integridade referencial */
 public class DeleteData {
     public static void main(String[] args) {
+        Connection connection;
+        PreparedStatement preparedStatement = null;
 
+        try {
+            connection = DB.getConnection();
+            String sql = "DELETE FROM department WHERE id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, 2);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
     }
 }
